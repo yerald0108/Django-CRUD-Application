2ab5c9e169991da3b10e551f8bd37bbc413da318
yerald0108 <yeraldfuste@gmail.com>
2025-11-30 12:22:42 +0100

    cero

---
+++ tasks/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError 
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html',)
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+        'from': UserCreationForm
+        })
+    else:
+            if request.POST['password1'] == request.POST['password2']:
+                # register user
+                try:
+                    user = User.objects.create_user(username=request.POST['username'], 
+                    password=request.POST['password1'])
+                    user.save()
+                    login(request, user)
+                    return redirect('tasks')
+                except IntegrityError:
+                    return render(request, 'signup.html', {
+                        'from': UserCreationForm,
+                        "error": 'Username already exists'
+                    })
+            return render(request, 'signup.html', {
+                        'from': UserCreationForm,
+                        "error": 'Password do not match'
+                    })
+
+def tasks(request):
+    return render(request, 'tasks.html')
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'from': AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Username or password is incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
